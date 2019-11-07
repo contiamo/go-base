6a506dad3a792fa7e1d002a7737ba89febb361bf
Denis Rechkunov <mail@pragmader.me>
2019-11-07 15:50:14 +0100

    Add private key read function to JWT config (#18)
    This will allow to configure JWT signing as well.

--- pkg/config/jwt.go
+++ pkg/config/jwt.go
@@ -12,6 +12,8 @@ import (
 type JWT struct {
 	// PublicKeyPath is a path to the public key for JWT signature verification
 	PublicKeyPath string `json:"publicKeyPath"`
+	// PrivateKeyPath is a path to the private key for signing JWT
+	PrivateKeyPath string `json:"privateKeyPath"`
 }
 
 // GetPublicKey gets the encryption key from a given path
@@ -26,4 +28,18 @@ func (j *JWT) GetPublicKey() (publicKey *rsa.PublicKey, err error) {
 	}
 
 	return jwt.ParseRSAPublicKeyFromPEM(keyBytes)
+}
+
+// GetPrivateKey gets the encryption key from a given path
+func (j *JWT) GetPrivateKey() (privateKey *rsa.PrivateKey, err error) {
+	if j.PrivateKeyPath == "" {
+		return nil, errors.New("path to the private key file is empty")
+	}
+
+	keyBytes, err := ioutil.ReadFile(j.PrivateKeyPath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "can not read the private key file `%s`", j.PrivateKeyPath)
+	}
+
+	return jwt.ParseRSAPrivateKeyFromPEM(keyBytes)
 }
