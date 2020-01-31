c0ecf929032406d432285da28f992059b92d1597
Denis Rechkunov <mail@pragmader.me>
2020-01-31 13:40:06 +0100

    Add Seal/Unseal and crypto token creation (#23)
    Now it's possible to use Authenticated Encryption using
    MAC-then-Encrypt (MtE) approach.
    
    Also, now it's easier to create a state or CSRF token using
    `GenerateToken` and then verify it using `DecodeAndVerifyToken`.

--- pkg/crypto/encrypt_test.go
+++ pkg/crypto/encrypt_test.go
@@ -17,7 +17,7 @@ func TestPassphraseToKey(t *testing.T) {
 	}
 }
 
-func Test_EncryptDecrypt(t *testing.T) {
+func TestEncryptDecrypt(t *testing.T) {
 	key := PassphraseToKey("some very secure passphrase no hacker can hack")
 	text := []byte("some very secret text to encrypt")
 
@@ -61,6 +61,63 @@ func Test_EncryptDecrypt(t *testing.T) {
 		_, err := Decrypt([]byte{0}, key)
 		if err != ErrCipherTooShort {
 			t.Fatal(err, "expected `ErrCipherTooShort`")
+		}
+	})
+}
+
+func TestSealUnseal(t *testing.T) {
+	key := PassphraseToKey("some very secure passphrase no hacker can hack")
+	text := []byte("some very secret text to encrypt")
+
+	t.Run("seals/unseals bytes", func(t *testing.T) {
+		cipherText, err := Seal(text, key)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if bytes.Equal(text, cipherText) {
+			t.Fatal("cipher text must differ the original text")
+		}
+
+		plainText, err := Unseal(cipherText, key)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !bytes.Equal(text, plainText) {
+			t.Fatal("decrypted text must match the original text")
+		}
+	})
+
+	t.Run("seals/unseals a string", func(t *testing.T) {
+		cipherTextString, err := SealToString(text, key)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if string(text) == cipherTextString {
+			t.Fatal("cipher text must differ the original text")
+		}
+
+		plainText, err := UnsealFromString(cipherTextString, key)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !bytes.Equal(text, plainText) {
+			t.Fatal("decrypted text must match the original text")
+		}
+	})
+
+	t.Run("authenticates the message", func(t *testing.T) {
+		cipherText, err := Seal(text, key)
+		if err != nil {
+			t.Fatal(err)
+		}
+		cipherText[1] = 'x'
+
+		plainText, err := Unseal(cipherText, key)
+		if err != ErrCorruptedMessage {
+			t.Fatal(err)
+		}
+		if len(plainText) != 0 {
+			t.Fatal("decrypted text must be empty in case of error")
 		}
 	})
 }
--- pkg/crypto/generate_test.go
+++ pkg/crypto/generate_test.go
@@ -1,6 +1,10 @@
 package crypto
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+	"time"
+)
 
 func TestRandomString(t *testing.T) {
 	t.Run("does not repeat the same value twice", func(t *testing.T) {
@@ -32,6 +36,44 @@ func TestRandomString(t *testing.T) {
 		}
 		if len(r1) != 15 {
 			t.Fatal("string length must be 15 characters")
+		}
+	})
+}
+
+func TestGenerateDecodeAndVerifyToken(t *testing.T) {
+	key := PassphraseToKey("some very secure passphrase no hacker can hack")
+	text := []byte("some very secret text to encrypt")
+
+	t.Run("preserves the data", func(t *testing.T) {
+		token, err := GenerateToken(text, key)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		data, err := DecodeAndVerifyToken(token, key, time.Hour)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !bytes.Equal(text, data) {
+			t.Fatal("data does not match with the initial text")
+		}
+	})
+
+	t.Run("expires", func(t *testing.T) {
+		token, err := GenerateToken(text, key)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		<-time.After(2 * time.Second)
+
+		data, err := DecodeAndVerifyToken(token, key, time.Second)
+		if err != ErrTokenExpired {
+			t.Fatal(err)
+		}
+		if len(data) != 0 {
+			t.Fatal("data should be empty")
 		}
 	})
 }
