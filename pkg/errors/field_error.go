32a25c422eff8a98ff5a5baa01f91e3fa4f02c73
Tino Rusch <tino.rusch@gmail.com>
2020-11-12 09:47:02 +0100

    order error response types alphabetically

--- pkg/errors/field_error.go
+++ pkg/errors/field_error.go
@@ -2,7 +2,7 @@ package errors
 
 // FieldError represents a validation error
 type FieldError struct {
-	GeneralError
 	// Key of the validated field
 	Key string `json:"key"`
+	GeneralError
 }
--- pkg/errors/general_error.go
+++ pkg/errors/general_error.go
@@ -2,8 +2,8 @@ package errors
 
 // GeneralError represents a system error exposed to the user
 type GeneralError struct {
-	// Type of the error
-	Type ErrorType `json:"type"`
 	// Message of the validation error
 	Message string `json:"message"`
+	// Type of the error
+	Type ErrorType `json:"type"`
 }
