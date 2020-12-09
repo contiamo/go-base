5275a1f1340303b2afc22325271034801b7f66f7
Lucas Roesler <roesler.lucas@gmail.com>
2020-12-09 14:34:15 +0100

    Implement JSONEq that allows matches on empty string (#75)
    **What**
    - To simplify some of the tests checks, it can be helpful to allow
      matches on two empty strings, even though neither is valid json.
      Specifically this helps when testing apis that can return errors
      and 204 empty body.
    
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

---
+++ pkg/testing/json.go
@@ -0,0 +1,26 @@
+package testing
+
+import (
+	"github.com/stretchr/testify/require"
+)
+
+type tHelper interface {
+	Helper()
+}
+
+// JSONEq asserts that two JSON strings are equivalent or both empty.
+//
+//  require.JSONEq(t, "", "")
+//  or
+//  require.JSONEq(t, `{"hello": "world", "foo": "bar"}`, `{"foo": "bar", "hello": "world"}`)
+func JSONEq(t require.TestingT, expected string, actual string, msgAndArgs ...interface{}) {
+	if h, ok := t.(tHelper); ok {
+		h.Helper()
+	}
+	// handle empty json
+	if expected == actual && expected == "" {
+		return
+	}
+
+	require.JSONEq(t, expected, actual, msgAndArgs...)
+}
