5fc1866565d3e43c37679a2157dc80d3e839bf2f
Lucas Roesler <roesler.lucas@gmail.com>
2021-05-11 11:07:56 +0200

    Fix linting errors
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

--- pkg/http/monitoring_test.go
+++ pkg/http/monitoring_test.go
@@ -14,14 +14,21 @@ func TestListenAndServeMonitoring(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 
-	go ListenAndServeMonitoring(ctx, ":8080", nil)
+	go func() {
+		err := ListenAndServeMonitoring(ctx, ":8080", nil)
+		require.NoError(t, err)
+	}()
 	// it takes some time to run the server, can't be accessed immediately
 	time.Sleep(100 * time.Millisecond)
 
-	resp, err := http.Get("http://localhost:8080/metrics")
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost:8080/metrics", nil)
+	require.NoError(t, err)
+
+	resp, err := http.DefaultClient.Do(req)
 	require.NoError(t, err)
 
 	defer resp.Body.Close()
-	bs, _ := ioutil.ReadAll(resp.Body)
+	bs, err := ioutil.ReadAll(resp.Body)
+	require.NoError(t, err)
 	require.Contains(t, string(bs), "go_info")
 }
