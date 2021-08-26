0c7c070069b23f4375d11d721418aa5e9bc63b28
Lucas Roesler <roesler.lucas@gmail.com>
2021-08-26 14:31:38 +0200

    chore: avoid logging in test goroutine after test finishes
    Split the test for the monitoring server shutdown to a separate test and
    remove the error check in the original test.
    
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

--- pkg/http/monitoring_test.go
+++ pkg/http/monitoring_test.go
@@ -15,8 +15,7 @@ func TestListenAndServeMonitoring(t *testing.T) {
 	defer cancel()
 
 	go func() {
-		err := ListenAndServeMonitoring(ctx, ":8080", nil)
-		require.NoError(t, err)
+		_ = ListenAndServeMonitoring(ctx, ":8080", nil)
 	}()
 	// it takes some time to run the server, can't be accessed immediately
 	time.Sleep(100 * time.Millisecond)
@@ -31,4 +30,20 @@ func TestListenAndServeMonitoring(t *testing.T) {
 	bs, err := ioutil.ReadAll(resp.Body)
 	require.NoError(t, err)
 	require.Contains(t, string(bs), "go_info")
+}
+
+func TestListenAndServeMonitoringShutdown(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+
+	done := make(chan error)
+	go func() {
+		err := ListenAndServeMonitoring(ctx, ":8080", nil)
+		done <- err
+	}()
+	// it takes some time to run the server, can't be accessed immediately
+	time.Sleep(100 * time.Millisecond)
+	cancel()
+
+	err := <-done
+	require.EqualError(t, err, http.ErrServerClosed.Error())
 }
