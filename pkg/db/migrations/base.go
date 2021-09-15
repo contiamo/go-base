892d1a863dd7bc66b557a69941f1a9d52b2a7a51
Lucas Roesler <roesler.lucas@gmail.com>
2021-09-15 10:17:51 +0200

    feat: use pg_advisory_lock to control db migration concurrency
    Using pg_advisory_lock allows us to block multiple application instances
    from running migrations at the same time. The current implementation
    uses a table lock, but this requires using transactions, which we have
    seen behave oddly. This approach allows us to avoid one large global
    transaction and we can move the transaction to wrap each individual
    statement instead of the entire process.
    
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

--- pkg/db/migrations/base.go
+++ pkg/db/migrations/base.go
@@ -1,6 +1,8 @@
 package migrations
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -27,4 +29,17 @@ func getSQL(name string, kind sqlKind, assets http.FileSystem) (string, error) {
 	}
 
 	return string(s), nil
+}
+
+// Lock to ensure multiple migrations cannot occur simultaneously
+const lockNum = int64(47831730) // arbitrary random number
+
+func acquireAdvisoryLock(ctx context.Context, db *sql.DB) error {
+	_, err := db.ExecContext(ctx, "select pg_advisory_lock($1)", lockNum)
+	return err
+}
+
+func releaseAdvisoryLock(ctx context.Context, db *sql.DB) error {
+	_, err := db.ExecContext(ctx, "select pg_advisory_unlock($1)", lockNum)
+	return err
 }
--- pkg/db/migrations/views.go
+++ pkg/db/migrations/views.go
@@ -24,23 +24,6 @@ func NewPostIniter(stmts []string, assets http.FileSystem) func(context.Context,
 }
 
 func configureViews(ctx context.Context, db *sql.DB, stmts []string, assets http.FileSystem) error {
-	tx, err := db.BeginTx(ctx, nil)
-	if err != nil {
-		return err
-	}
-
-	defer func() {
-		if err == nil {
-			err = tx.Commit()
-			return
-		}
-
-		rollbackErr := tx.Rollback()
-		if rollbackErr != nil {
-			err = errors.Wrap(err, rollbackErr.Error())
-		}
-	}()
-
 	for i, stmtName := range stmts {
 		logger := logrus.WithField("stmt", stmtName)
 		logger.Debug("migration started")
@@ -50,12 +33,11 @@ func configureViews(ctx context.Context, db *sql.DB, stmts []string, assets http
 			logger.Errorf("migration failed: %v", err)
 			return fmt.Errorf("migration failed: %w", err)
 		}
-
-		_, err = tx.ExecContext(ctx, stmt)
+		err = runStatement(ctx, db, stmt)
 		if err != nil {
 			return errors.Wrapf(err, "configure view failed at index %d", i)
 		}
 	}
 
-	return err
+	return nil
 }
