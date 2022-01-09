1c83a04622134efae59111a4ce6e3852b1eaa855
Lucas Roesler <roesler.lucas@gmail.com>
2022-01-09 15:10:15 +0100

    feat: add Getters to the component Info struct
    Adding Getters allows us to pass the component.Info as an interface and
    share it with other packages.
    
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

--- pkg/otel/component/info.go
+++ pkg/otel/component/info.go
@@ -6,3 +6,15 @@ type Info struct {
 	Version string `json:"version"`
 	Commit  string `json:"commit"`
 }
+
+func (i Info) GetName() string {
+	return i.Name
+}
+
+func (i Info) GetVersion() string {
+	return i.Version
+}
+
+func (i Info) GetCommit() string {
+	return i.Commit
+}
