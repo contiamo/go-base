f0829894de291cbe8b28cb3706ec7216f7ed09da
Lucas Roesler <roesler.lucas@gmail.com>
2022-04-19 13:10:07 +0200

    feat: allow skipping http loggin per request
    Provide configuration points to allow application to skip logging on
    specific requests. Additionally, allow configuration of the log fields
    as well.
    
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

--- pkg/http/middlewares/logging_test.go
+++ pkg/http/middlewares/logging_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 
@@ -35,7 +36,11 @@ func Test_LoggingMiddleware(t *testing.T) {
 		require.NoError(t, err)
 		defer resp.Body.Close()
 
-		require.Contains(t, buf.String(), "successfully handled request")
+		data := buf.String()
+		require.Equal(t, 1, strings.Count(data, "\n"))
+		require.Contains(t, data, `level=info`)
+		require.Contains(t, data, `msg="successfully handled request"`)
+		require.Contains(t, data, `app=test duration_millis=0 method=GET path=/logging status_code=200`)
 	})
 
 	t.Run("should support websockets", func(t *testing.T) {
@@ -52,6 +57,61 @@ func Test_LoggingMiddleware(t *testing.T) {
 		require.NoError(t, err)
 
 		time.Sleep(100 * time.Millisecond)
-		require.Contains(t, buf.String(), "successfully handled request")
+		data := buf.String()
+		require.Equal(t, 1, strings.Count(data, "\n"))
+		require.Contains(t, data, `level=info`)
+		require.Contains(t, data, `msg="successfully handled request"`)
+		require.Contains(t, data, `app=test duration_millis=0 method=GET path=/ws/echo status_code=200`)
+	})
+
+	t.Run("should ignore healthchecks", func(t *testing.T) {
+		buf, restore := utils.SetupLoggingBuffer()
+		defer restore()
+
+		srv, err := createServer([]server.Option{WithLogging("test")})
+		require.NoError(t, err)
+
+		ts := httptest.NewServer(srv.Handler)
+		defer ts.Close()
+
+		req, err := http.NewRequest(http.MethodGet, ts.URL+"/logging", nil)
+		require.NoError(t, err)
+		req = req.WithContext(ctx)
+		req.Header.Set("User-Agent", "ELB-HealthChecker")
+
+		resp, err := http.DefaultClient.Do(req)
+		require.NoError(t, err)
+		defer resp.Body.Close()
+
+		require.Equal(t, 200, resp.StatusCode)
+
+		require.Equal(t, buf.String(), "")
+	})
+
+	t.Run("should log on error codes", func(t *testing.T) {
+		buf, restore := utils.SetupLoggingBuffer()
+		defer restore()
+
+		srv, err := createServer([]server.Option{WithLogging("test")})
+		require.NoError(t, err)
+
+		ts := httptest.NewServer(srv.Handler)
+		defer ts.Close()
+
+		req, err := http.NewRequest(http.MethodGet, ts.URL+"/error", nil)
+		require.NoError(t, err)
+		req = req.WithContext(ctx)
+
+		resp, err := http.DefaultClient.Do(req)
+		require.NoError(t, err)
+		defer resp.Body.Close()
+
+		require.Equal(t, 400, resp.StatusCode)
+
+		data := buf.String()
+		require.Equal(t, 1, strings.Count(data, "\n"), data)
+		require.Contains(t, data, `level=warn`)
+		require.Contains(t, data, `msg="problem while handling request"`)
+		require.Contains(t, data, `app=test duration_millis=0 method=GET path=/error status_code=400`)
 	})
 }
